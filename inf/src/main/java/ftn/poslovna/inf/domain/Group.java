9d6f8052ebc2b857b5cefd733daa3cc4899848ee
GrYloX <goranrajic95@gmail.com>
2018-07-10 10:26:13 +0200

    magarac

--- inf/src/main/java/ftn/poslovna/inf/domain/Group.java
+++ inf/src/main/java/ftn/poslovna/inf/domain/Group.java
@@ -9,8 +9,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="Grupa")
 public class Group {
 	
 	@Id
